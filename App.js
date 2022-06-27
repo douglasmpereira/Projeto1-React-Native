aaff2b9aaf387d8c8c657f837f92db3b3c7146c6
douglasmpereira <d-moreira18@hotmail.com>
2022-06-27 11:55:26 -0300

    projeto funcional

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Sup} from './componentes/blocosup';
+import { Quad1 } from './componentes/quadrado1';
+import { Ret1 } from './componentes/retangulo1';
+import { Ret2 } from './componentes/retangulo2';
+import { Quad2 } from './componentes/quadrado2';
+import { Ret3 } from './componentes/retangulo3';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1 }}>
+     
+        <Sup/>
+        <Quad1/> 
+        <Ret1/> 
+        <Ret2/> 
+        <Quad2/>
+        <Ret3/>
+      
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
